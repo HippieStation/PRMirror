c2cc265a17031923b30d8706e8b97159d6eef456
Chnkr <Chnkr@users.noreply.github.com>
2017-03-11 00:35:09 +0100

    Implement proper logging
    Signed-off-by: Chnkr <Chnkr@users.noreply.github.com>

--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"context"
-	"log"
-
-	"fmt"
-
 	"encoding/json"
+	"os"
 
 	"github.com/google/go-github/github"
+	"github.com/op/go-logging"
 	"golang.org/x/oauth2"
 )
 
 var Configuration = Config{}
 
+var log = logging.MustGetLogger("PRMirror")
+
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
 func main() {
+
+	backend := logging.NewLogBackend(os.Stderr, "", 0)
+	backendFormatter := logging.NewBackendFormatter(backend, format)
+	logging.SetBackend(backendFormatter)
+
 	Configuration = Configuration.Init()
 
 	ctx := context.Background()
@@ -26,7 +35,8 @@ func main() {
 	client := github.NewClient(tc)
 	events, _, err := client.Activity.ListRepositoryEvents(ctx, Configuration.UpstreamOwner, Configuration.UpstreamRepo, nil)
 	if _, ok := err.(*github.RateLimitError); ok {
-		log.Println("hit rate limit")
+		// TODO: Maybe add some context here
+		log.Error("The github.com rate limit has been hit")
 	}
 
 	for _, event := range events {
@@ -41,7 +51,7 @@ func main() {
 
 			prAction := prEvent.GetAction()
 
-			fmt.Printf("%s\n", prEvent.PullRequest.GetURL())
+			log.Debugf("%s", prEvent.PullRequest.GetURL())
 
 			if prAction == "opened" {
 				//TODO: Check if we already have an open PR for this and add a comment saying upstream reopened it and remove the upsteam closed tag
